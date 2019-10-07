8354a3884659b966d0dee02a6279e59f0b56ac9e
DONaumenko <dmitry.naumenko@gmail.com>
2019-10-07 21:23:36 +0300

    2dz

--- variables.py
+++ variables.py
@@ -1,11 +1,9 @@
 name = 'tomato'
-print(type(name))
-
 quantity_of_tomatoes = 12
-print(type(quantity_of_tomatoes))
-
 avg_size = 6.7
-print(type(avg_size))
-
 is_fresh = True
+
+print(type(name))
+print(type(quantity_of_tomatoes))
+print(type(avg_size))
 print(type(is_fresh))
